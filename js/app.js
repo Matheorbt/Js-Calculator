5a971edfee246ea94bcd07d5347e328b0bdf5774
MATHEO ROBERT <matho.robert@gmail.com>
2021-02-08 14:49:20 +0100

    Sign handling added and number positionement changed

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ const calculator = document.querySelector(".calculator");
 
 const screen = document.querySelector(".off");
 const textresult = document.querySelector(".screenNb");
+const lastNb = document.querySelector(".lastNb");
 
 const floatDot = document.querySelector(".float");
 const equal = document.querySelector(".equal");
@@ -17,8 +18,13 @@ const divisionSign = document.querySelector(".division");
 const multiplicationSign = document.querySelector(".multiplication");
 const substractionSign = document.querySelector(".substraction");
 const moduloSign = document.querySelector(".modulo");
+const sign = document.querySelectorAll(".sign:not(.equal,.float)");
 
 const numbers = document.querySelectorAll('[data-nb]');
+
+var firstNb = 0;
+var secondNb = 0;
+var result = 0;
 
 const switchCalculator = () => {
     power.addEventListener("click", () => {
@@ -26,6 +32,7 @@ const switchCalculator = () => {
         screen.classList.toggle("screen");
         isOn = !isOn;
         textresult.innerHTML = '';
+        lastNb.innerHTML = '';
     });
 };
 
@@ -91,6 +98,26 @@ const getResult = () => {
     });
 };
 
+const handleOperator = () => {
+    sign.forEach(Element => {
+        Element.addEventListener('click', event => {
+            firstNb = parseInt(textresult.textContent);
+            lastNb.innerHTML = textresult.textContent + ' ' + Element.textContent;
+            textresult.innerHTML = ''; 
+        })
+    });
+};
+
+const handleEqual = () => {
+    equal.addEventListener("click", () => {
+        secondNb = parseInt(textresult.textContent);
+        lastNb.innerHTML = '';
+        textresult.innerHTML = result;
+    });
+};
+
+handleEqual();
+handleOperator();
 switchCalculator();
 putNb();
 addition();
